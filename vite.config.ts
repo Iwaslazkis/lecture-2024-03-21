7ada74ffdf5ad73e1cc64dd1d68f5be57a2ba601
Your Name <you@example.com>
2024-04-11 18:15:27 +0000

    Configure build

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(() => {
+  if (process.env.BUILD_COMBINED === 'true' && process.env.BUILD_TYPE === 'storybook') {
+    return {
+      plugins: [react()],
+      base: "/storybook"
+    }    
+  } else {
+    return {
+      plugins: [react()]
+    }
+  }
+
 })
